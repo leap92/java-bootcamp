e4ffcd6e1e37854db5a3283484e5fa94d50a0c87
Leandro Pross <leacap10@gmail.com>
2015-09-14 17:17:13 -0300

    Main can now be instantiated from Eclipse - now using JDK 1.8

--- Topic 2/simpleMavenProject/src/main/java/com/leap92/bootcamp/App.java
+++ Topic 2/simpleMavenProject/src/main/java/com/leap92/bootcamp/App.java
@@ -1,5 +1,6 @@
 package com.leap92.bootcamp;
 
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -9,8 +10,13 @@ public class App
 {
     public static void main( String[] args )
     {
-    	System.out.println("Escriba su nombre:");
-        String input = System.console().readLine();
-        System.out.println("Hola " + input);
+    	if (System.console() == null) {
+    	      Scanner sc = new Scanner(System.in);
+    	      System.out.println("Hola " + sc.next());
+    	      sc.close();
+    	     } else {
+    	      String input = System.console().readLine();
+    	      System.out.println("Hola " + input);
+    	     }
     }
 }
